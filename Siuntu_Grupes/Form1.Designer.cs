6f6399f87c7879deb0006fedb739fc6263ac1822
Sinicel <audrius.sinica@gmail.com>
2020-05-24 20:38:36 +0300

    Paleisti programa per centra ekrano.

--- Siuntu_Grupes/Form1.Designer.cs
+++ Siuntu_Grupes/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 52);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(1343, 401);
             this.dataGridView1.TabIndex = 0;
@@ -137,6 +138,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
